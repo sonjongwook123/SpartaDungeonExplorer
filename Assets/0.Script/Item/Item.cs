c3ab7696f3dc62c2a9a3cdfc950dffff8a677a06
sonjongwook123 <sonjongwook123@gmail.com>
2025-05-22 04:37:24 +0900

    아이템 데이터

---
+++ Assets/0.Script/Item/Item.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [Header("전략 아이템 스크립트")]
+    [Tooltip("이 오브젝트가 나타낼 아이템 데이터 ScriptableObject를 할당.")]
+    [SerializeField] private ItemEffectSO itemData;
+
+
+}
--- Assets/0.Script/Item/ItemObject.cs
+++ Assets/0.Script/Item/ItemObject.cs
@@ -1,18 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemObject : MonoBehaviour
+public interface ItemEffect
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void ApplyEffect(Transform target);
+}
 
-    // Update is called once per frame
-    void Update()
+public abstract class ItemEffectSO : ScriptableObject, ItemEffect
+{
+    [Header("Basic Item Info")]
+    public string itemName = "New Item";
+    public string description = "A generic item.";
+    public abstract void ApplyEffect(Transform target);
+}
+
+[CreateAssetMenu(fileName = "ConsumableItem", menuName = "Item Effects/HP restore")]
+public class HpItem : ItemEffectSO
+{
+    [SerializeField] private int restoreAmount = 10;
+
+    public override void ApplyEffect(Transform target)
     {
-        
+        //target 체력 회복
     }
 }
+
+[CreateAssetMenu(fileName = "SpecialItem", menuName = "Item Effects/HP minus")]
+public class HpMinusItem : ItemEffectSO
+{
+    [SerializeField] private int restoreAmount = 10;
+
+    public override void ApplyEffect(Transform target)
+    {
+        //target 체력 감소
+    }
+}
+
+[CreateAssetMenu(fileName = "ConsumableItem", menuName = "Item Effects/Stamina restore")]
+public class StaminaItem : ItemEffectSO
+{
+    [SerializeField] private int restoreAmount = 10;
+
+    public override void ApplyEffect(Transform target)
+    {
+        //target 스테미너 회복
+    }
+}
+
+[CreateAssetMenu(fileName = "JustItem", menuName = "Items/No Effect")]
+public class JustItem : ItemEffectSO
+{
+    public override void ApplyEffect(Transform target)
+    {
+        Debug.Log("just item");
+    }
+}
+
+[CreateAssetMenu(fileName = "ConsumableItem", menuName = "Item Effects/Double Jump")]
+public class JumpItem : ItemEffectSO
+{
+    [SerializeField] private int restoreAmount = 10;
+
+    public override void ApplyEffect(Transform target)
+    {
+        //target 더블 점프
+    }
+}
+
